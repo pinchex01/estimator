5a28ae25a35f01b2bc2700106b5cce23fa8916d7
pete <ptrmutie@gmail.com>
2020-04-13 18:19:22 +0300

    challenge one

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,58 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => data => {
+    let estimatedFactor = 10;
+
+    const duration = (period, days) => {
+        switch (period) {
+          case 'weeks':
+            return days * 7;
+          case 'months':
+            return days * 30;
+          default:
+            return days;
+        }
+      };
+
+    const estimator = chain (
+        //challenge one
+        estimateCurrentlyInfected = (reportedCases) => {
+            return reportedCases * estimatedFactor;
+          },
+
+        estimateProjectedInfections = () => {
+            return reportedCases * 50;
+        },
+
+        estimateInfectionssByRequestedTime = () => {
+            estimateCurrentlyInfected * 1024;
+        },
+
+        //challenge two
+        estimatedSevereCases = () => {
+
+        },
+
+        estimateBedSpaceAvailability = () => {
+
+        },
+
+        //challenge three
+        estimateCasesForICU = () => {
+
+        },
+        estimateCasesForVentilators = () => {
+
+        },
+        estimateDollarsInFlight = () => {
+
+        }
+    );
+
+    return estimator ({
+        data,
+        impact: {},
+        severImpact: {}
+    });
+};
+
 
 export default covid19ImpactEstimator;
