22418a3fdea1e53e7a0ffa9006d70540b8764319
pete <ptrmutie@gmail.com>
2020-04-13 20:22:24 +0300

    just something

--- src/estimator.js
+++ src/estimator.js
@@ -1,37 +1,41 @@
+import * as utils from './utils';
+
 const covid19ImpactEstimator = (data) => {
+  const { reportedCases: cases, totalHospitalBeds, region } = data;
+  const { avgDailyIncomeInUSD: avgUSD, avgDailyIncomePopulation: avgDailyIncomePpltn } = region;
+  const { bestCase: iCurrentlyInfected, worstCase: sCurrentlyInfected } = utils.CalcInfected(cases);
+  const iInfections = iCurrentlyInfected * (2 ** utils.CalculateFactor(data));
+  const sInfections = sCurrentlyInfected * (2 ** utils.CalculateFactor(data));
+  const iSevereCasesByRequestedTime = Math.trunc(0.15 * (iInfections));
+  const sSevereCasesByRequestedTime = Math.trunc(0.15 * (sInfections));
+  const iAvailableBeds = Math.trunc((0.35 * totalHospitalBeds) - iSevereCasesByRequestedTime);
+  const sAvailableBeds = Math.trunc((0.35 * totalHospitalBeds) - sSevereCasesByRequestedTime);
+  const iCasesForICUByRequestedTime = Math.trunc(0.05 * iInfections);
+  const sCasesForICUByRequestedTime = Math.trunc(0.05 * sInfections);
+  const iCasesForVentilatorsByRequestedTime = Math.trunc(0.02 * iInfections);
+  const sCasesForVentilatorsByRequestedTime = Math.trunc(0.02 * sInfections);
+  const iDollars = (iInfections * avgDailyIncomePpltn * avgUSD) / utils.formatDuration(data);
+  const sDollars = (sInfections * avgDailyIncomePpltn * avgUSD) / utils.formatDuration(data);
 
-    currentlyInfected = (reportedCases) => {
-        return reportedCases * 10;
+  return {
+    data,
+    impact: {
+      currentlyInfected: iCurrentlyInfected,
+      infectionsByRequestedTime: iInfections,
+      severeCasesByRequestedTime: iSevereCasesByRequestedTime,
+      hospitalBedsByRequestedTime: iAvailableBeds,
+      casesForICUByRequestedTime: iCasesForICUByRequestedTime,
+      casesForVentilatorsByRequestedTime: iCasesForVentilatorsByRequestedTime,
+      dollarsInFlight: Math.trunc(iDollars)
+    },
+    severeImpact: {
+      currentlyInfected: sCurrentlyInfected,
+      infectionsByRequestedTime: sInfections,
+      severeCasesByRequestedTime: sSevereCasesByRequestedTime,
+      hospitalBedsByRequestedTime: sAvailableBeds,
+      casesForICUByRequestedTime: sCasesForICUByRequestedTime,
+      casesForVentilatorsByRequestedTime: sCasesForVentilatorsByRequestedTime,
+      dollarsInFlight: Math.trunc(sDollars)
     }
-
-    severeImpacted = (reportedCases) => {
-        return reportedCases * 50;
-    }
-    
-    infectionsByRequestedTime = () => {
-        currentlyInfected * 1024;
-    }
-    // const estimator = chain (
-    //     //challenge one
-    //     estimateCurrentlyInfected,
-    //     estimateProjectedInfections,
-
-    //     //challenge two
-    //     estimatedSevereCases,
-    //     estimateBedSpaceAvailability,
-
-    //     //challenge three
-    //     estimateCasesForICU,
-    //     estimateCasesForVentilators,
-    //     estimateDollarsInFlight
-    // );
-
-    return estimator ({
-        data,
-        impact: {},
-        severImpact: {}
-    });
+  };
 };
-
-
-export default covid19ImpactEstimator;
